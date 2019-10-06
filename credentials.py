17239fe0a17771f8c8b269079e9cf00490c52b72
Jay-68 <kelvinngari1995@gmail.com>
2019-10-06 18:19:18 +0300

    add a method for deleting an existing profile

--- credentials.py
+++ credentials.py
@@ -16,3 +16,20 @@ class Credentials:
         Saves user credentials to the credentials_list
         '''
         Credentials.credentials_list.append(self)
+
+    @classmethod
+    def credentials_exists(cls, account_name):
+        '''
+        Checks if there is an already existing account
+        '''
+        for credentials in cls.credentials_list:
+            if credentials.account_name == account_name:
+                return True
+            else:
+                return False
+
+    def delete_credentials(self):
+        '''
+        deletes an existing profile
+        '''
+        Credentials.credentials_list.remove(self)
