3e0733903134e18a0978a9c6f508249b5a9a8884
Jay-68 <kelvinngari1995@gmail.com>
2019-10-06 15:00:33 +0300

    create a test for the user class

---
+++ user_test.py
@@ -0,0 +1,24 @@
+import unittest
+from user import User
+
+
+class test_user(unittest.TestCase):
+    '''
+    This test class defines test cases for the behavior of the User class.
+    '''
+
+    def setUp(self):
+        '''
+        The set up method to run each test case.
+        '''
+        self.new_user = User(
+            'James Ngari', 'ngari.james.n@gmail.com', 'Jay-68', '123456')
+
+    def test_init(self):
+        '''
+        This test checks if the new user has been initialized with the parameters in the class User.
+        '''
+        self.assertEqual(self.new_user.fullname, 'James Ngari')
+        self.assertEqual(self.new_user.email, 'ngari.james.n@gmail.com')
+        self.assertEqual(self.new_user.username, 'Jay-68')
+        self.assertEqual(self.new_user.password, '123456')
