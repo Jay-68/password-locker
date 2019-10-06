0bcc450ac9e67ff81ab9d66300d99bcf0b4839d3
Jay-68 <kelvinngari1995@gmail.com>
2019-10-06 12:58:09 +0300

    Create user and credentials class

---
+++ credentials.py
@@ -0,0 +1,13 @@
+class Credentials:
+    '''
+    This class creates new instances of the class Credentials
+    '''
+
+    def __init__(self, account_name, email, username, password):
+        self.account_name = account_name
+        self.email = email
+        self.username = username
+        self.password = password
+
+
+cred_list = []
---
+++ user.py
@@ -0,0 +1,13 @@
+class User:
+    '''
+    This class generates new user instances
+    '''
+
+    def __init__(self, fullname, email, username, password):
+        self.fullname = fullname
+        self.email = email
+        self.username = username
+        self.password = password
+
+
+user_list = []
