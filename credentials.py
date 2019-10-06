09cb63102756e8a61335a622f1d423952cd09e0d
Jay-68 <kelvinngari1995@gmail.com>
2019-10-06 18:41:40 +0300

    create a class method to find account by account name

--- credentials.py
+++ credentials.py
@@ -33,3 +33,25 @@ class Credentials:
         deletes an existing profile
         '''
         Credentials.credentials_list.remove(self)
+
+    @classmethod
+    def display_accounts(cls):
+        '''
+        Displays the credentials of a username
+        '''
+        return cls.credentials_list
+
+    @classmethod
+    def find_by_account_name(cls, account_name):
+        '''
+        Method that takes in an account name and returns the credentials that match that account name.
+        Args:
+            account_name: Account name to search for
+        Returns :
+            Full credentials that matches the account_name.
+        '''
+        for credentials in cls.credentials_list:
+            if credentials.account_name==account_name:
+                return credentials
+    @classmethod
+    def generate_password(cls,username)
